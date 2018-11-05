f47d1cdc0622302e282a8c0cf3333af902aef8e6
Saad Mohmed <saadm7625@gmail.com>
2018-11-05 16:12:42 +0200

    dit number 18

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 //Install express server
 const express = require('express');
+const http = require('http');
 const path = require('path');
 
 const app = express();
-
+const port = process.env.PORT || 3001;
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/new-blog'));
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
     // Replace the '/dist/<to_your_project_name>/index.html'
-    res.sendFile(path.join(__dirname + '/dist/new-blog/index.html'));
+    res.sendFile(path.join(__dirname +'/dist/new-blog/index.html'));
 });
+const server = http.createServer(app);
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+server.listen(port , ()=>{
+    console.log('runniing ??')
+});
