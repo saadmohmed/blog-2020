b58196d604f5f5fc6a8d6f4f880825687062a6f6
Saad Mohmed <saadm7625@gmail.com>
2018-11-06 03:29:18 +0200

    first commit

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/new-blog'));
+app.use(express.static(__dirname + '/dist/new-blog/'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.get('/*', function (req, res) {
+    // Replace the '/dist/<to_your_project_name>/index.html'
+    res.sendFile(path.join(__dirname +'/dist/new-blog/src/index.html'));
+});
 
+app.listen(process.env.PORT || 8080);
 
 
 // //Install express server
